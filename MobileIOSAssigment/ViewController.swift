16b406e901b1c6d4560ba58da52ada0085c9f699
Mehmet Gul <mehmetgul_@outlook.com>
2023-06-15 10:59:32 +0300

    Initial Commit

---
+++ MobileIOSAssigment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobileIOSAssigment
+//
+//  Created by Mehmet Gül on 15.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
