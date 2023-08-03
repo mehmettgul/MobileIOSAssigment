b3118ffd845d02513bed2ae9a8fb86599bd65b99
Mehmet Gül <mehmetgul_@outlook.com>
2023-08-03 14:35:45 +0300

    listView uptated

--- MobileIOSAssigment/ListViewController.swift
+++ MobileIOSAssigment/ListViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class ListViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UISearchBarDelegate{
     
@@ -13,9 +14,8 @@ class ListViewController: UIViewController, UICollectionViewDelegate, UICollecti
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var listCollectionView: UICollectionView!
     
-    var listData: [dataResponse] = DataManager.shared.dataArray
-    var searchData: [dataResponse] = []
-    var isSearching: Bool = false
+    var listData: [dataResponse] = []
+    let baseUrl = "https://pixabay.com/api/"
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         // her bir item'ın boyutunu belirlediğimiz fonksiyon.
@@ -28,15 +28,18 @@ class ListViewController: UIViewController, UICollectionViewDelegate, UICollecti
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // kaç section olacağıyla alakalı method
-        return isSearching ? searchData.count : listData.count // kaç eleman olacağını döndürür.
+        return listData.count // kaç eleman olacağını döndürür.
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        // Cell'lerin içerisindeki verileri göstermeye yarayan method
+        
+        let item = listData[indexPath.row]
+        
+        guard !listData.isEmpty && indexPath.row < listData.count else {
+            return UICollectionViewCell() // Geçerli indeks aralığından dışarıda ise boş bir hücre döndürün.
+        }
         
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "listItem", for: indexPath) as? ListItemCollectionViewCell else { return UICollectionViewCell() }
-        
-        let item = searchData[indexPath.row]
         
         cell.getWidthHeightListItem(width: item.previewWidth, height: item.previewHeight)
         if let imageURL = URL(string: item.previewURL) {
@@ -48,15 +51,12 @@ class ListViewController: UIViewController, UICollectionViewDelegate, UICollecti
         return cell
     }
     
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        if searchText.isEmpty {
-            isSearching = false
-            searchData = []
-        } else {
-            isSearching = true
-            searchData = listData.filter{$0.user.lowercased().contains(searchText.lowercased())}
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        if let query = searchBar.text, !query.isEmpty {
+            listData.removeAll() // Mevcut verileri temizleyin.
+            fetchData(with: query)
+            searchBar.resignFirstResponder()
         }
-        listCollectionView.reloadData()
     }
     
     override func viewDidLoad() {
@@ -75,4 +75,50 @@ class ListViewController: UIViewController, UICollectionViewDelegate, UICollecti
         listCollectionView.setCollectionViewLayout(layout, animated: true)
         print(listData)
     }
+    
+    func fetchData(with query: String) {
+        let _: [String: Any] = [
+            "key": "37427312-d8d1e5548011abcd225706ea7",
+            "q": query // Bu, q parametresiyle arama sorgusunu belirlemek için eklenir.
+        ]
+        
+        AF.request("\(baseUrl)?key=37427312-d8d1e5548011abcd225706ea7&q=\(query)", method: .get).responseJSON { response in
+            if let error = response.error { // hata alıp almadığımızı kontrol ediyoruz.
+                print(error)
+            } else { // hata yoksa çektiğimiz verileri konsoldan görebiliyoruz.
+                do {
+                    if let data = response.data {
+                        if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                            // JSON verisi başarıyla parse edildi, verilere eriş
+                            if let fetchData = json["hits"] as? [[String: Any]] {
+                                    for data in fetchData { // her bir nesnenin doğru türden olup olmadığını kontrol ediyor.
+                                    if  let id = data["id"] as? Int,
+                                        let previewURL = data["previewURL"] as? String,
+                                        let previewWidth = data["previewWidth"] as? Int,
+                                        let previewHeight = data["previewHeight"] as? Int,
+                                        let likes = data["likes"] as? Int,
+                                        let comments = data["comments"] as? Int,
+                                        let views = data["views"] as? Int,
+                                        let user = data["user"] as? String,
+                                        let userImageURL = data["userImageURL"] as? String,
+                                        let webformatURL = data["webformatURL"] as? String,
+                                        let webformatWidth = data["webformatWidth"] as? Int,
+                                        let webformatHeight = data["webformatHeight"] as? Int {
+                                        let responseData = dataResponse(id: id, previewURL: previewURL, previewWidth: previewWidth, previewHeight: previewHeight, likes: likes, comments: comments, views: views, user: user, userImageURL: userImageURL, webformatURL: webformatURL, webformatWidth: webformatWidth, webformatHeight: webformatHeight)
+                                        self.listData.append(responseData)
+                                    }
+                                }
+                            }
+                        } else {
+                            print("else'deki error")
+                        }
+                    }
+                } catch {
+                    print("catch'deki error")
+                }
+                self.listCollectionView.reloadData()
+            }
+        }
+    }
+    
 }
